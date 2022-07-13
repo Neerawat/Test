01081128092ba5eae6214eb21fb742836dc093ac
Neerawat <emo.negative69@gmail.com>
2022-07-13 22:59:53 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//--1--
+/*console.log("PostNews")
+    console.log("2")
+    alert("News!!")
+
+    var name = "Nine"+"55"
+    console.log(name)
+
+    name = "SS"
+    console.log(name)
+
+    var firstname = "Neerawat"
+    var lastname = "Sriphum"
+    console.log(firstname)
+    console.log(lastname)
+
+    alert(name+" - "+firstname+"  "+lastname)*/
+
+//--2--
+   /*var a = "ASD"
+   alert(a)
+   var age = 12
+   console.log(typeof a)
+   console.log(typeof age)
+   console.log(typeof true)
+   console.log(typeof 15.69)*/
+
+//--3--
+/*var i1 = 2
+    var i2 = 3
+    var i3 = 4
+    var age = 12
+    console.log(i1+i2*i3)
+    console.log(4+2*2/2*2+4+1-5+19)+91
+    alert(age)
+    {
+        var x = 20
+        let y = 30  
+        alert(y)
+    }
+    alert(x)*/
+    
+
+    //--4--
+    /*var x =20
+{
+    var x = 100
+    let y = 15
+}
+console.log(x)*/
+
+//--5--
+/*var a = "Test"
+var name = prompt("กรุณากรอกข้อความ")
+console.log(name)
+alert(a)*/
+
+var message = prompt("กรุณาใส่ข่าวสาร")
+console.log(message)
+document.getElementById("news").innerHTML = message
